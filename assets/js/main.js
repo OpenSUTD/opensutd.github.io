2d1928101875951e399690da2628c24f900b8018
James R T <jamestiotio@gmail.com>
2019-10-16 03:35:38 +0800

    Fix text formatting & remove #toTop button

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,16 +32,3 @@ $(document).on("click", 'a[href^="#"]', function(event) {
         500
     );
 });
-
-// When the user scrolls down 20px from the top of the document, show the scroll up button
-window.onscroll = function() {
-    scrollFunction();
-};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("toTop").style.display = "block";
-    } else {
-        document.getElementById("toTop").style.display = "none";
-    }
-}
