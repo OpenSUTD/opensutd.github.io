e42a9df07b653c2921a002b1088d20275609db3b
chesnutcase <chester8991@gmail.com>
2019-10-31 18:45:19 +0800

    give everyone a clickable anchor link

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,36 @@
 // Get that hamburger menu cookin' //
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Get all "navbar-burger" elements
-    var $navbarBurgers = Array.prototype.slice.call(
-        document.querySelectorAll(".navbar-burger"),
-        0
-    );
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-        // Add a click event on each of them
-        $navbarBurgers.forEach(function($el) {
-            $el.addEventListener("click", function() {
-                // Get the target from the "data-target" attribute
-                var target = $el.dataset.target;
-                var $target = document.getElementById(target);
-                // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-                $el.classList.toggle("is-active");
-                $target.classList.toggle("is-active");
-            });
-        });
-    }
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function($el) {
+      $el.addEventListener("click", function() {
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
 });
 
 // Smooth Anchor Scrolling
 $(document).on("click", 'a[href^="#"]', function(event) {
-    event.preventDefault();
-    $("html, body").animate(
-        {
-            scrollTop: $($.attr(this, "href")).offset().top
-        },
-        500
-    );
+  event.preventDefault();
+  $("html, body").animate({
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    500,
+    "swing",
+    // animation callback – when finished, update the address bar
+    () => (window.location = event.target.href)
+  );
 });
